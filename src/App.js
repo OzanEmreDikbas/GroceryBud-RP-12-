a34288ca0fae09f9192fd778f51c5bbeeb96a455
Ozan Emre Dikbaş <87207946+OzanEmreDikbas@users.noreply.github.com>
2021-12-24 14:16:29 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react'
+import List from './List'
+import Alert from './Alert'
+
+function App() {
+  const [name, setName] = useState('');
+  const [list, setList] = useState(getLocalStorage());
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+}
+}
+
+export default App
